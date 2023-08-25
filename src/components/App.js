2ea3029b0e157fb793a13f36f54c8a6b7378a1fb
Wilson Wanjiru <wilsonmuita41@gmail.com>
2023-08-25 16:03:14 +0300

    <React Children>

--- src/components/App.js
+++ src/components/App.js
@@ -4,19 +4,20 @@ import Container from "./Container";
 function App() {
   return (
     <div className="App">
-      <Container header="Example header" direction="horizontal">
-        <div>This is an example!</div>
-        <div>This is an example!</div>
-        <div>This is an example!</div>
-        <div>This is an example!</div>
-        <div>This is an example!</div>
-        <div>This is an example!</div>
-        <div>This is an example!</div>
+      <Container header="Pragramming Languages" direction="horizontal">
+        <div>React</div>
+        <div>JavaScript</div>
+        <div>Python</div>
+        <div>Java</div>
+        <div>PHP</div>
+        <div>C#</div>
+        <div>CSS</div>
       </Container>
       <Container header="Example header" textPosition="center">
-        <img
-          alt="turtle"
-          src="https://pbs.twimg.com/profile_images/3022993403/fdae67d800527328cee7a222f048dbbd.jpeg"
+      <img
+          alt="React"
+          src="https://e0.pxfuel.com/wallpapers/183/962/desktop-wallpaper-how-to-build-a-search-app-with-react-using-the-unsplash-api-digitalocean-react-native.jpg"
+          className="small-image"
         />
         <img
           alt="turtle"
